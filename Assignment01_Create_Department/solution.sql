63ca8b4c241e4ac0cfa021b20fde70fef73a3965
vasukiravikumar55-afk <vasukiravikumar55@gmail.com>
2026-07-17 12:45:44 +0530

    Update solution.sql

--- Assignment01_Create_Department/solution.sql
+++ Assignment01_Create_Department/solution.sql
@@ -1,4 +1,6 @@
--- Write your SQL here
+use collegedbbca;
+CREATE TABLE DEPARTMENT (department_ID int PRIMARY KEY, department_name VARCHAR(20),HOD VARCHAR(20));
+SELECT* FROM DEPARTMENT;
 
 -- Create Database
 
