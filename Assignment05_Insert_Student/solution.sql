73108c47bc95b9a8c17b09a6844043583b9d0c72
vasukiravikumar55-afk <vasukiravikumar55@gmail.com>
2026-08-25 09:32:07 +0530

    Update solution.sql

--- Assignment05_Insert_Student/solution.sql
+++ Assignment05_Insert_Student/solution.sql
@@ -11,13 +11,7 @@ CREATE TABLE Student(
     Email VARCHAR(30),
     PhoneNumber BIGINT
 );
-
--- Insert
-
--- 1001 Arun
-
--- 1002 Divya
-
--- 1003 Karthik
-
--- Display all records
+use collegedbbca;
+CREATE TABLE course (courseID VARCHAR(20),coursename VARCHAR (20), credits INT (20) PRIMARY KEY, departmentID VARCHAR(20));
+INSERT INTO course(courseID, coursename, credits, departmentID) VALUES (10,"BCA",12,1);
+DESC course;
