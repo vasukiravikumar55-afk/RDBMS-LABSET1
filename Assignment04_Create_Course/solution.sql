0255270c3bab40378128b0a2395b2469116a3646
vasukiravikumar55-afk <vasukiravikumar55@gmail.com>
2026-08-25 09:31:01 +0530

    Update solution.sql

--- Assignment04_Create_Course/solution.sql
+++ Assignment04_Create_Course/solution.sql
@@ -1,9 +1,7 @@
 DROP DATABASE IF EXISTS CollegeDB;
 CREATE DATABASE CollegeDB;
 USE CollegeDB;
-
--- Create Course table
-
--- Insert three records
-
--- Display structure
+use collegedbbca;
+CREATE TABLE course (courseID VARCHAR(20),coursename VARCHAR (20), credits INT (20) PRIMARY KEY, departmentID VARCHAR(20));
+INSERT INTO course(courseID, coursename, credits, departmentID) VALUES (10,"BCA",12,1);
+DESC course;
