ecc2d74d6cc91f3089b109f1f06e94b3fb8603ed
vasukiravikumar55-afk <vasukiravikumar55@gmail.com>
2026-08-27 11:17:49 +0530

    Update solution.sql

--- Assignment09_Inner_Join/solution.sql
+++ Assignment09_Inner_Join/solution.sql
@@ -2,11 +2,40 @@ DROP DATABASE IF EXISTS CollegeDB;
 CREATE DATABASE CollegeDB;
 USE CollegeDB;
 
+INSERT INTO Department (DepartmentID, DepartmentName)
+VALUES (101, 'Computer Science');
 
--- Create Department
+INSERT INTO Department (DepartmentID, DepartmentName)
+VALUES (102, 'Mathematics');
+
+INSERT INTO Department (DepartmentID, DepartmentName)
+VALUES (103, 'Physics');
 
 -- Create Student
+CREATE TABLE Student (
+    StudentID INT,
+    StudentName VARCHAR(50),
+    DepartmentID INT
+);
 
--- Insert sample records
+
+-- Insert sample records (student)
+
+INSERT INTO Student (StudentID, StudentName, DepartmentID)
+VALUES (1001, 'Arun', 101);
+
+INSERT INTO Student (StudentID, StudentName, DepartmentID)
+VALUES (1002, 'Divya', 102);
+
+INSERT INTO Student (StudentID, StudentName, DepartmentID)
+VALUES (1003, 'Karthik', 101);
+
+INSERT INTO Student (StudentID, StudentName, DepartmentID)
+VALUES (1004, 'Nisha', 103);
 
 -- INNER JOIN query
+
+SELECT Student.StudentName, Department.DepartmentName
+FROM Student
+INNER JOIN Department
+ON Student.DepartmentID = Department.DepartmentID;
