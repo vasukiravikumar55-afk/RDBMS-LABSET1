957cdc995679f87bc930141cbd4a120e1194406b
vasukiravikumar55-afk <vasukiravikumar55@gmail.com>
2026-08-27 11:25:13 +0530

    Update solution.sql

--- Assignment10_Left_Right_Join/solution.sql
+++ Assignment10_Left_Right_Join/solution.sql
@@ -2,63 +2,50 @@ DROP DATABASE IF EXISTS CollegeDB;
 CREATE DATABASE CollegeDB;
 USE CollegeDB;
 
+-- Create Department
 
--- Create Course
-
-CREATE TABLE Course (
-    CourseID INT,
-    CourseName VARCHAR(50),
-    Credits INT
+CREATE TABLE Department (
+    DepartmentID INT,
+    DepartmentName VARCHAR(50)
 );
 
+-- Insert sample records (department)
 
--- Insert sample records (course)
+INSERT INTO Department (DepartmentID, DepartmentName)
+VALUES (101, 'Computer Science');
 
-INSERT INTO Course (CourseID, CourseName, Credits)
-VALUES (201, 'Database Systems', 4);
+INSERT INTO Department (DepartmentID, DepartmentName)
+VALUES (102, 'Mathematics');
 
-INSERT INTO Course (CourseID, CourseName, Credits)
-VALUES (202, 'Data Structures', 3);
+INSERT INTO Department (DepartmentID, DepartmentName)
+VALUES (103, 'Physics');
 
-INSERT INTO Course (CourseID, CourseName, Credits)
-VALUES (203, 'Mathematics', 4);
-
-
--- Create Enrollment
-
-CREATE TABLE Enrollment (
-    EnrollmentID INT,
+-- Create Student
+CREATE TABLE Student (
     StudentID INT,
-    CourseID INT
+    StudentName VARCHAR(50),
+    DepartmentID INT
 );
 
 
--- Insert sample records (enrollment)
+-- Insert sample records (student)
 
-INSERT INTO Enrollment (EnrollmentID, StudentID, CourseID)
-VALUES (1, 1001, 201);
+INSERT INTO Student (StudentID, StudentName, DepartmentID)
+VALUES (1001, 'Arun', 101);
 
-INSERT INTO Enrollment (EnrollmentID, StudentID, CourseID)
-VALUES (2, 1001, 202);
+INSERT INTO Student (StudentID, StudentName, DepartmentID)
+VALUES (1002, 'Divya', 102);
 
-INSERT INTO Enrollment (EnrollmentID, StudentID, CourseID)
-VALUES (3, 1002, 203);
+INSERT INTO Student (StudentID, StudentName, DepartmentID)
+VALUES (1003, 'Karthik', 101);
 
-INSERT INTO Enrollment (EnrollmentID, StudentID, CourseID)
-VALUES (4, 1003, 201);
+INSERT INTO Student (StudentID, StudentName, DepartmentID)
+VALUES (1004, 'Nisha', 103);
 
+-- INNER JOIN query
 
--- LEFT JOIN
+SELECT Student.StudentName, Department.DepartmentName
+FROM Student
+INNER JOIN Department
+ON Student.DepartmentID = Department.DepartmentID;
 
-SELECT *
-FROM Course
-LEFT JOIN Enrollment
-ON Course.CourseID = Enrollment.CourseID;
-
-
--- RIGHT JOIN
-
-SELECT *
-FROM Course
-RIGHT JOIN Enrollment
-ON Course.CourseID = Enrollment.CourseID;
