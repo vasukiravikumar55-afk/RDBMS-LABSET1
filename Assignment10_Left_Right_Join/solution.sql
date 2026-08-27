1e3612152c7700e6d60d1837d4b917a82b156a01
vasukiravikumar55-afk <vasukiravikumar55@gmail.com>
2026-08-27 11:19:16 +0530

    Update solution.sql

--- Assignment10_Left_Right_Join/solution.sql
+++ Assignment10_Left_Right_Join/solution.sql
@@ -2,12 +2,63 @@ DROP DATABASE IF EXISTS CollegeDB;
 CREATE DATABASE CollegeDB;
 USE CollegeDB;
 
+
 -- Create Course
+
+CREATE TABLE Course (
+    CourseID INT,
+    CourseName VARCHAR(50),
+    Credits INT
+);
+
+
+-- Insert sample records (course)
+
+INSERT INTO Course (CourseID, CourseName, Credits)
+VALUES (201, 'Database Systems', 4);
+
+INSERT INTO Course (CourseID, CourseName, Credits)
+VALUES (202, 'Data Structures', 3);
+
+INSERT INTO Course (CourseID, CourseName, Credits)
+VALUES (203, 'Mathematics', 4);
+
 
 -- Create Enrollment
 
--- Insert sample records
+CREATE TABLE Enrollment (
+    EnrollmentID INT,
+    StudentID INT,
+    CourseID INT
+);
+
+
+-- Insert sample records (enrollment)
+
+INSERT INTO Enrollment (EnrollmentID, StudentID, CourseID)
+VALUES (1, 1001, 201);
+
+INSERT INTO Enrollment (EnrollmentID, StudentID, CourseID)
+VALUES (2, 1001, 202);
+
+INSERT INTO Enrollment (EnrollmentID, StudentID, CourseID)
+VALUES (3, 1002, 203);
+
+INSERT INTO Enrollment (EnrollmentID, StudentID, CourseID)
+VALUES (4, 1003, 201);
+
 
 -- LEFT JOIN
 
+SELECT *
+FROM Course
+LEFT JOIN Enrollment
+ON Course.CourseID = Enrollment.CourseID;
+
+
 -- RIGHT JOIN
+
+SELECT *
+FROM Course
+RIGHT JOIN Enrollment
+ON Course.CourseID = Enrollment.CourseID;
