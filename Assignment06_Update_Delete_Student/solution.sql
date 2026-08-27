93c751caa0832862900efd5c6e75a79ea989d2c7
vasukiravikumar55-afk <vasukiravikumar55@gmail.com>
2026-08-27 10:29:39 +0530

    Update solution.sql

--- Assignment06_Update_Delete_Student/solution.sql
+++ Assignment06_Update_Delete_Student/solution.sql
@@ -14,8 +14,8 @@ CREATE TABLE Student(
 
 INSERT INTO Student(StudentID,StudentName,Gender,DepartmentID)
 VALUES
-(1001,'Arun','Male',101),
-(1002,'Divya','Female',102),
+(1001,'Arun','Male',101);
+(1002,'Divya','Female',102);
 (1003,'Karthik','Male',101);
 
 -- Update Karthik's DepartmentID
