f60842eae7320863ddeb2b1a3ef63e81934c51d9
vasukiravikumar55-afk <vasukiravikumar55@gmail.com>
2026-08-27 11:15:54 +0530

    Update solution.sql

--- Assignment08_Aggregate_Functions/solution.sql
+++ Assignment08_Aggregate_Functions/solution.sql
@@ -5,11 +5,37 @@ USE CollegeDB;
 -- Create Employee table
 
 -- Insert records
+INSERT INTO Employee (EmployeeID, EmployeeName, Department, Salary)
+VALUES (101, 'Ravi', 'HR', 25000);
+
+INSERT INTO Employee (EmployeeID, EmployeeName, Department, Salary)
+VALUES (102, 'Meena', 'IT', 40000);
+
+INSERT INTO Employee (EmployeeID, EmployeeName, Department, Salary)
+VALUES (103, 'Kumar', 'Finance', 35000);
+
+INSERT INTO Employee (EmployeeID, EmployeeName, Department, Salary)
+VALUES (104, 'Suresh', 'IT', 45000);
+
+INSERT INTO Employee (EmployeeID, EmployeeName, Department, Salary)
+VALUES (105, 'Latha', 'HR', 30000);
+
 
 -- COUNT()
 
+SELECT COUNT(Salary) AS Total_Employees FROM Employee;
+
 -- MAX()
+
+SELECT MAX(Salary) AS Maximum_Salary FROM Employee;
+
 
 -- MIN()
 
+SELECT MIN(Salary) AS Minimum_Salary FROM Employee;
+
+
 -- AVG()
+
+SELECT AVG(Salary) AS Average_Salary FROM Employee;
+
